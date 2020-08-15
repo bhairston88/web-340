08e09f5cf9bbf8b890690f0eb10bff1bf02d694d
Brooklyn Hairston <bhairston@my365.bellevue.edu>
2020-08-15 13:26:31 -0400

    adding exercise 2.2

---
+++ week-2/hello-world/hairston-hello-world-express.js
@@ -0,0 +1,30 @@
+/*
+============================================
+; Title: hairston-hello-world-express.js
+; Author: Professor Massoud
+; Date: 15 August 2020
+; Modified By: Brooklyn Hairston
+; Description: Demonstrates
+;===========================================
+*/
+
+//header
+const header = require('../../hairston-header.js')
+
+console.log(header.display('Brooklyn','Hairston','Hello World with Express'))
+
+//start program
+
+var express = require("express");
+
+var http = require("http");
+
+var app = express();
+
+app.use(function(request, response) {
+    console.log("In comes a request to:" + request.url);
+    
+    response.end("Hello World");
+});
+
+http.createServer(app).listen(8080);
