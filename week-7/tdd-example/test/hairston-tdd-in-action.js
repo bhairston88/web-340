159ba901e08fc337fa1527b54c0268c80833c19a
Brooklyn Hairston <bhairston@my365.bellevue.edu>
2020-09-20 10:48:15 -0400

    adding exercise 7.2

---
+++ week-7/tdd-example/test/hairston-tdd-in-action.js
@@ -0,0 +1,28 @@
+/*
+============================================
+; Title: hairston-tdd-in-action.js
+; Author: Professor Massoud
+; Date: 20 September 2020
+; Modified By: Brooklyn Hairston
+; Description: Demonstrates how to create and use TDD testing
+;===========================================
+*/
+
+//header
+const header = require('../hairston-header')
+
+console.log(header.display('Brooklyn','Hairston','TDD in ACtion'))
+
+//start program
+
+//require statement
+var assert = require("assert");
+
+//unit test
+describe("String#split", function() {
+    it("should return an array of fruits", function() {
+        assert(Array.isArray("Apple, Orange, Mango".split(",")));
+    });
+});
+
+//end program
